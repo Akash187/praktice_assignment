5c2223f2f3307a7ddf33655053ccd298d1250683
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-01-07 22:51:50 +0530

    initial setup completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Picker from "react-mobile-picker";
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    data: {},
+    valueGroups: {
+      title: "Mr.",
+      firstName: "Micheal",
+      secondName: "Jordan"
+    },
+    optionGroups: {
+      title: ["Mr.", "Mrs.", "Ms.", "Dr."],
+      firstName: ["John", "Micheal", "Elizabeth"],
+      secondName: ["Lennon", "Jackson", "Jordan", "Legend", "Taylor"]
+    }
+  };
+
+  componentDidMount(){
+    fetch('./data.json').then(response => {
+      console.log(response);
+      return response.json();
+    }).then(data => {
+      // Work with JSON data here
+      this.setState({data});
+    }).catch(err => {
+      // Do something for an error here
+      console.log("Error Reading data " + err);
+    });
+  }
+
+  // Update the value in response to user picking event
+  handleChange = (name, value) => {
+    this.setState(({ valueGroups }) => ({
+      valueGroups: {
+        ...valueGroups,
+        [name]: value
+      }
+    }));
+  };
+
   render() {
+    const {data, optionGroups, valueGroups } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2 style={{textAlign: "center"}}>{data.title}</h2>
+        <Picker
+          optionGroups={optionGroups}
+          valueGroups={valueGroups}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
